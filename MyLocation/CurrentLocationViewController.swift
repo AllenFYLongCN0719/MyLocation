f642f0b21025f8a3307a0b51b83fdd0bbe52f0b9
Long Fu Yu <33461058+AllenFYLong@users.noreply.github.com>
2018-01-08 22:20:44 +0800

    1/8/2018 Updated
    Added locationManager delegate.

--- MyLocation/CurrentLocationViewController.swift
+++ MyLocation/CurrentLocationViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import CoreLocation
 
-class CurrentLocationViewController: UIViewController {
+class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var latitudeLabel: UILabel!
@@ -17,8 +18,12 @@ class CurrentLocationViewController: UIViewController {
     @IBOutlet weak var tagButton: UIButton!
     @IBOutlet weak var getButton: UIButton!
     
+    let locationManager = CLLocationManager()
+    
     @IBAction func getLocation() {
-        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.startUpdatingLocation()
     }
     
     override func viewDidLoad() {
@@ -30,7 +35,16 @@ class CurrentLocationViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //MARK: - CLLoctionManagerDelegate
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("didFailWithError \(error)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let newLocation = locations.last!
+        print("didUpdatelocations \(newLocation)")
+    }
 
 }
 
