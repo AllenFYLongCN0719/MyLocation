fdcc1a776854aa9c32ddf4ca0413c65ff7a7662a
Long Fu Yu <33461058+AllenFYLong@users.noreply.github.com>
2018-01-10 22:04:50 +0800

    1/10/2018 Updated
    Added new func showLocationServicesDeniedAlert() and updated func getlocation()

--- MyLocation/CurrentLocationViewController.swift
+++ MyLocation/CurrentLocationViewController.swift
@@ -21,6 +21,18 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
     let locationManager = CLLocationManager()
     
     @IBAction func getLocation() {
+        let authStatus = CLLocationManager.authorizationStatus()
+        //检查目前的许可状态，如果时禁止访问位置信息的话，app会字啊用户使用时进行许可请求
+        if authStatus == .notDetermined {
+            locationManager.requestWhenInUseAuthorization()
+            return
+        }
+        
+        if authStatus == .denied || authStatus == .restricted {
+            showLocationServicesDeniedAlert()
+            return
+        }
+        
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
         locationManager.startUpdatingLocation()
@@ -44,6 +56,14 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let newLocation = locations.last!
         print("didUpdatelocations \(newLocation)")
+    }
+    
+    func showLocationServicesDeniedAlert() {
+        //这个弹出窗口会展示一条有用的信息，如果无法获取位置信息的话，这个app就是无用的，所以应该鼓励用户打开location服务的授权
+        let alert = UIAlertController(title: "Location Services Disabled", message: "Please enable location services for this app in Settings.", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okAction)
+        present(alert,animated: true,completion: nil)
     }
 
 }
